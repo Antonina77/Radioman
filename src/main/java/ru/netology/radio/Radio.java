3272226d2e6d9c1f17e2a34ba1c2c5232e03265c
Antonina <adrianery94@mail.ru>
2020-12-08 14:48:28 +0300

    added changes commit

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -2,102 +2,98 @@ package ru.netology.radio;
 
 public class Radio {
 
+    private int maxStation = 10;
+    private int minStation = 0;
     private int currentStation;
-    private int maxStation;
-    private int minStation;
+    private int maxSound = 100;
+    private int minSound = 0;
     private int currentSound;
-    private int minSound;
-    private int maxSound;
+
+    public Radio() {
+    }
+
+    public Radio(int maxStation, int minStation, int currentStation, int maxSound, int minSound, int currentSound) {
+        this.maxStation = maxStation;
+        this.minStation = minStation;
+        this.currentStation = currentStation;
+        this.maxSound = maxSound;
+        this.minSound = minSound;
+        this.currentSound = currentSound;
+
+    }
 
     public int getCurrentStation() {
         return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > getMaxStation() || currentStation < getMinStation()) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
     }
 
     public int getCurrentSound() {
         return currentSound;
     }
 
-    public void setCurrentSound(int currentSound) {
-        if (currentSound > getMaxSound() || currentSound < getMinSound()) {
-            return;
-        }
-        this.currentSound = currentSound;
-    }
-
-    public int getMaxSound() {
-        return maxSound;
-    }
-
-    public void setMaxVolume(int maxSound) {
-        this.maxSound = maxSound;
-    }
-
-    public int getMinSound() {
-        return minSound;
-    }
-
-    public void setMinVolume(int minSound) {
-        this.minSound = minSound;
-    }
-
-
     public void increaseCurrentStation() {
-        if (currentStation >= maxStation) {
-            currentStation = 0;
+        if (currentStation < maxStation && currentStation >= minStation) {
+            this.currentStation++;
             return;
+        } else {
+            currentStation = minStation;
         }
-        this.currentStation++;
-    }
 
+    }
 
     public void decreaseCurrentStation() {
-        if (currentStation <= minStation) {
-            currentStation = 9;
+        if (currentStation > minStation && currentStation <= maxStation) {
+            this.currentStation--;
             return;
+        } else {
+            currentStation = maxStation;
         }
-        this.currentStation--;
     }
 
+    public void inputCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            this.currentStation = minStation;
+            return;
+        } else if (currentStation < minStation) {
+            this.currentStation = maxStation;
+            return;
+        }
+        this.currentStation = currentStation;
+    }
 
     public void increaseCurrentSound() {
-        if (currentSound >= maxSound) {
-            currentSound = 10;
+        if (currentSound < maxSound && currentSound >= minSound) {
+            this.currentSound++;
             return;
+        } else if (currentSound <= minSound) {
+            this.currentSound = minSound;
+            return;
+        } else {
+            this.currentSound = maxSound;
         }
-        this.currentSound++;
     }
 
-
     public void decreaseCurrentSound() {
-        if (currentSound <= minSound) {
-            currentSound = 0;
+        if (currentSound > minSound && currentSound <= maxSound) {
+            this.currentSound--;
             return;
+        } else if (currentSound >= maxSound) {
+            this.currentSound = maxSound;
+            return;
+        } else {
+            this.currentSound = minSound;
         }
-        this.currentSound--;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -2,18 +2,14 @@ package ru.netology.radio;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/DataForIncreaseStation.csv", delimiter = '|', numLinesToSkip = 2)
-    void shouldIncreaseCurrentStation(int currentStation, int minStation, int maxStation, int expected) {
-        Radio radio = new Radio();
-        radio.setMaxStation(maxStation);
-        radio.setMinStation(minStation);
-        radio.setCurrentStation(currentStation);
+    void shouldIncreaseCurrentStation(int currentStation, int expected) {
+        Radio radio = new Radio(10, 0, currentStation, 100, 0, 0);
         radio.increaseCurrentStation();
 
         assertEquals(expected, radio.getCurrentStation());
@@ -22,23 +18,25 @@ class RadioTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/DataForDecreaseStation.csv", delimiter = '|', numLinesToSkip = 2)
-    void shouldDecreaseCurrentStation(int currentStation, int minStation, int maxStation, int expected) {
-        Radio radio = new Radio();
-        radio.setMaxStation(maxStation);
-        radio.setMinStation(minStation);
-        radio.setCurrentStation(currentStation);
+    void shouldDecreaseCurrentStation(int currentStation, int expected) {
+        Radio radio = new Radio(10, 0, currentStation, 100, 0, 0);
         radio.decreaseCurrentStation();
 
         assertEquals(expected, radio.getCurrentStation());
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/DataForIncreaseSound.csv", delimiter = '|', numLinesToSkip = 2)
-    void shouldIncreaseCurrentVolume(int currentVolume, int minVolume, int maxVolume, int expected) {
+    @CsvFileSource(resources = "/DataForInputStation.csv", delimiter = '|', numLinesToSkip = 2)
+    void shouldInputCurrentStation(int currentStation, int expected) {
         Radio radio = new Radio();
-        radio.setMaxVolume(maxVolume);
-        radio.setMinVolume(minVolume);
-        radio.setCurrentSound(currentVolume);
+        radio.inputCurrentStation(currentStation);
+        assertEquals(expected, radio.getCurrentStation());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/DataForIncreaseSound.csv", delimiter = '|', numLinesToSkip = 2)
+    void shouldIncreaseCurrentSound(int currentSound, int expected) {
+        Radio radio = new Radio(10, 0, 0, 100, 0, currentSound);
         radio.increaseCurrentSound();
 
         assertEquals(expected, radio.getCurrentSound());
@@ -46,11 +44,8 @@ class RadioTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/DataForDecreaseSound.csv", delimiter = '|', numLinesToSkip = 2)
-    void shouldDecreaseCurrentSound(int currentSound, int minSound, int maxSound, int expected) {
-        Radio radio = new Radio();
-        radio.setMaxVolume(maxSound);
-        radio.setMinVolume(minSound);
-        radio.setCurrentSound(currentSound);
+    void shouldDecreaseCurrentSound(int currentSound, int expected) {
+        Radio radio = new Radio(10, 0, 0, 100, 0, currentSound);
         radio.decreaseCurrentSound();
 
         assertEquals(expected, radio.getCurrentSound());
